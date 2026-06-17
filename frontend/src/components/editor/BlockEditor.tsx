28b827b724d1cdd4b084cea7b2a3dc7dc45a93ab
EDOHWARES <edohemmanuel4real@gmail.com>
2026-06-17 12:37:56 +0100

    feat: implement drag-and-drop block palette in editor

--- frontend/src/components/editor/BlockEditor.tsx
+++ frontend/src/components/editor/BlockEditor.tsx
@@ -8,11 +8,22 @@ import ReactFlow, {
   useEdgesState,
   useNodesState,
   type Connection,
+  type ReactFlowInstance,
 } from "reactflow"
 import "reactflow/dist/style.css"
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 import Toolbar from "./Toolbar"
 import DeployButton from "./DeployButton"
+import BlockNode from "./BlockNode"
+
+const nodeTypes = {
+  Condition: BlockNode,
+  Transfer: BlockNode,
+  Storage: BlockNode,
+  Event: BlockNode,
+  Auth: BlockNode,
+  default: BlockNode,
+}
 
 const initialNodes = [
   {
@@ -26,28 +37,73 @@ const initialNodes = [
 export default function BlockEditor() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState([])
+  const [reactFlowInstance, setReactFlowInstance] = useState<ReactFlowInstance | null>(null)
 
   const onConnect = useCallback(
     (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
     [setEdges]
   )
 
+  const onDragOver = useCallback((event: React.DragEvent) => {
+    event.preventDefault()
+    event.dataTransfer.dropEffect = "move"
+  }, [])
+
+  const onDrop = useCallback(
+    (event: React.DragEvent) => {
+      event.preventDefault()
+
+      if (!reactFlowInstance) return
+
+      const type = event.dataTransfer.getData("application/blocktype")
+
+      if (typeof type === "undefined" || !type) {
+        return
+      }
+
+      // Convert screen coordinates to flow coordinates
+      const position = reactFlowInstance.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      })
+
+      const newNode = {
+        id: `node_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`,
+        type,
+        position,
+        data: { label: type },
+      }
+
+      setNodes((nds) => nds.concat(newNode))
+    },
+    [reactFlowInstance, setNodes]
+  )
+
   return (
     <div className="relative h-full w-full">
       <Toolbar />
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
+      <div 
+        className="w-full h-full"
+        onDragOver={onDragOver}
+        onDrop={onDrop}
       >
-        <Background />
-        <Controls />
-        <MiniMap />
-      </ReactFlow>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onInit={setReactFlowInstance}
+          nodeTypes={nodeTypes}
+          fitView
+        >
+          <Background />
+          <Controls />
+          <MiniMap />
+        </ReactFlow>
+      </div>
       <DeployButton nodes={nodes} edges={edges} />
     </div>
   )
 }
+
